dde8bb9be99f8686c55ffdc9324db044e94c93ec
Galdin Raphael <gldraphael@users.noreply.github.com>
2017-12-14 22:44:18 +0530

    Fix service worker registration on build
    Use the correct directory for service worker generation

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var swPrecache = require('sw-precache');
 
 gulp.task('generate-sw', function(callback) {
   swPrecache.write('service-worker.js', {
-    staticFileGlobs: ['_site/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'],
-    stripPrefix: "_site",
+    staticFileGlobs: ['dist/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'],
+    stripPrefix: "dist",
     navigateFallback: '/'
   }, callback);
 });
