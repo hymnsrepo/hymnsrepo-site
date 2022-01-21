8d82686c79c877e6c078a5cdb0c133cfc14001a8
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-01-21 12:08:40 +0530

    update dependencies (#23)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 const gulp = require('gulp');
 const workbox = require('workbox-build');
-const ghPages = require('@justeat/gulp-gh-pages');
 
 gulp.task('generate-sw', () => {
   return workbox.generateSW({
-    globDirectory: 'public',
+    cacheId: "hymnsrepo",
+    globDirectory: "./public",
     globPatterns: [
-      '**/*.{js,json,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2}'
+        "**/*.{js,json,html,css,png,jpg,gif,svg,eot,ttf,woff,woff2,otf}"
     ],
-    swDest: 'public/sw.js',
+    swDest: "./public/sw.js",
+    modifyURLPrefix: {
+        "": "/"
+    },
     clientsClaim: true,
-    skipWaiting: true
+    skipWaiting: true,
+    ignoreURLParametersMatching: [/./],
+    offlineGoogleAnalytics: true,
+    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MiB
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+        handler: "CacheFirst",
+        options: {
+            cacheName: "google-fonts",
+            expiration: {
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+                maxEntries: 30
+            },
+        },
+      },
+    ],
   }).then(({warnings}) => {
     // In case there are any warnings from workbox-build, log them.
     for (const warning of warnings) {
@@ -22,6 +41,3 @@ gulp.task('generate-sw', () => {
   });
 });
  
-gulp.task('deploy', () => 
-  gulp.src('./public/**/*').pipe(ghPages())
-);
